001b720950d49927256dbed5b559ec0ecaee1966
FaridFatchur <m.farid.fatchur.rohmad-2021@fst.unair.ac.id>
2023-10-08 16:31:49 +0700

    Navbar-1

--- project/settings.py
+++ project/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/img/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
